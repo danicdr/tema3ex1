a71d38e3e19af6f52b9790a16499588de8f6848a
Daniel Codreanu <danielcdr2006@gmail.com>
2023-04-22 23:31:32 +0300

    Add project files.

---
+++ tema3ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//Scrieti o functie care va calcula suma cifrelor unui numar.
+namespace MyFirstProgram2._1
+{
+    class Program
+    {
+        static int SumaCifrelor(int numar)
+        {
+            int suma = 0;
+            while (numar != 0)
+            {
+                suma += numar % 10;
+                numar /= 10;
+            }
+            return suma;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar:");
+            int numar = int.Parse(Console.ReadLine());
+            int suma = SumaCifrelor(numar);
+            Console.WriteLine(suma);
+        }
+    }
+}
